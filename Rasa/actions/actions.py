9704cd6f6d4f52197b09cf79f98efe06bb7b7d09
FelicitasTheFurry <felix.rasch.16@outlook.com>
2025-02-14 12:45:44 +0100

    Neuer Funktioneller Rasa Bot

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -6,24 +6,24 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SessionStarted, ActionExecuted
-from rasa_sdk.executor import CollectingDispatcher
+#from typing import Any, Text, Dict, List
+#from rasa_sdk import Action, Tracker
+#from rasa_sdk.events import SessionStarted, ActionExecuted
+#from rasa_sdk.executor import CollectingDispatcher
 
-class ActionSessionStart(Action):
-    def name(self) -> Text:
-        return "action_session_start"
-
-    async def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
-    ) -> List[Dict[Text, Any]]:
-        # Neue Session initialisieren
-        events = [SessionStarted()]
-        
-        # Begrüßungsnachricht senden (verwende hier dein Template aus domain.yml)
-        dispatcher.utter_message(response="utter_greet")
-        
-        # Den Bot wieder in den Zuhörmodus versetzen
-        events.append(ActionExecuted("action_listen"))
-        return events
+#class ActionSessionStart(Action):
+#    def name(self) -> Text:
+#        return "action_session_start"
+#
+#    async def run(
+#        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+#    ) -> List[Dict[Text, Any]]:
+#        # Neue Session initialisieren
+#        events = [SessionStarted()]
+#        
+#        # Begrüßungsnachricht senden (verwende hier dein Template aus domain.yml)
+#        dispatcher.utter_message(response="utter_greet")
+#        
+#        # Den Bot wieder in den Zuhörmodus versetzen
+#        events.append(ActionExecuted("action_listen"))
+#        return events
